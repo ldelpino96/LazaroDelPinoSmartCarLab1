0b8cdb8e851e44ab1139249267849a939d0e6719
lazarodelpino <lazarodelpino@localhost>
2021-12-04 11:28:27 -0500

    SelfCheck

--- src/com/smartcarlab/SmartCar.java
+++ src/com/smartcarlab/SmartCar.java
@@ -1,20 +1,57 @@
 package com.smartcarlab;
 
-public abstract class SmartCar {
+import edu.fiu.sysdesign.SelfCheckCapable;
+import edu.fiu.sysdesign.SelfCheckUtils;
+
+public class SmartCar implements SelfCheckCapable {
 	private String color;
 	private String Model;
 	private int weight;
 	private int dimensions;
 	private int NumberofWheels;
-	public abstract int moveForward();
-	public abstract int moveBackward();
-	public abstract int stop();
-	public abstract int turnRight();
-	public abstract int turnLeft();
-	public abstract int spin();
+	private Battery powersource;
+	private Camera recorder;
+	private CPU computer;
+	private Lights illumination;
+	private Motors motion;
+	private Sensor detector;
+	private Speaker sound;
+	private Wheels bearings;
+	
+	public SmartCar() {
+		
+		powersource = new Battery();
+		recorder = new Camera();
+		computer = new CPU();
+		illumination = new Lights();
+		motion = new Motors();
+		detector = new Sensor();
+		sound = new Speaker();
+		bearings = new Wheels();
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		SmartCar mySmartCar = new SmartCar();
+		mySmartCar.runSelfCheck();
+	}
+	@Override
+	public boolean selfCheck() {
+		// TODO Auto-generated method stub
+		return SelfCheckUtils.randomCheck(0.1);
+	}
+	
+	@Override
+	public String getComponentName() {
+		// TODO Auto-generated method stub
+		return "Check completed";
 	}
 
+	@Override
+	public boolean runSelfCheck() {
+		// TODO Auto-generated method stub
+		return SelfCheckUtils.basicSelfCheckRunner(this);
+	}
 }
+
+
